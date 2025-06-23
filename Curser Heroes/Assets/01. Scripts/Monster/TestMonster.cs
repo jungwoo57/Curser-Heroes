75a144f1d1a65e50b61a56b2cb8e4e36336403bc
jellujoa <jellujoa03@gmail.com>
2025-06-23 20:13:10 +0900

    update testmonster

--- Curser Heroes/Assets/01. Scripts/Monster/TestMonster.cs
+++ Curser Heroes/Assets/01. Scripts/Monster/TestMonster.cs
@@ -3,17 +3,11 @@
 public class TestMonster : BaseMonster
 {
     public GameObject TestMonsterPrefab; // Inspector에서 지정
-    public Transform firePoint;       // 발사 위치
+    public Transform TestMonsterTransform;       // 발사 위치
 
     protected override void Attack()
     {
-        if (TestMonsterPrefab == null || firePoint == null) return;
 
-        GameObject fireball = Instantiate(TestMonsterPrefab, firePoint.position, Quaternion.identity);
-
-        Rigidbody2D rb = fireball.GetComponent<Rigidbody2D>();
-        if (rb != null)
-            rb.velocity = transform.right * 5f;
     }
 
     protected override void PlayHitEffect()
