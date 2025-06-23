fc7f825cb9d80a075b29b9019824a9ed36a92a54
kookin09 <hankookin12382@naver.com>
2025-06-23 14:25:46 +0900

    add: 웨이브 관련 코드 작성
    웨이브 관련 코드 작성
    WaveBuilder.cs
    WaveDate.cs
    WaveManager.cs
    GameManager.cs

---
+++ Curser Heroes/Assets/01. Scripts/Stage/Wave/WaveBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class WaveBuilder
+{
+    public static List<MonsterData> BuildWave(WaveData waveData, List<MonsterData> monsterPool)
+    {
+        int waveValue = waveData.waveValue;
+        int valueRange = waveData.GetValueRange();
+
+        int usedValue = 0;
+        List<MonsterData> spawnQueue = new List<MonsterData>();
+
+        while (usedValue < waveValue)
+        {
+            List<MonsterData> valid = monsterPool.FindAll(m => m.spawnValue <= valueRange && m.spawnValue + usedValue <= waveValue);
+
+            if (valid.Count == 0) break;
+
+            MonsterData selected = valid[Random.Range(0, valid.Count)];
+            spawnQueue.Add(selected);
+            usedValue += selected.spawnValue;
+        }
+
+        return spawnQueue;
+    }
+}
---
+++ Curser Heroes/Assets/01. Scripts/Stage/Wave/WaveManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    public List<MonsterData> allMonsters;
+    public List<WaveData> waveList;
+    public Transform[] spawnPoints;
+    public GameManager gameManager;
+
+    private int currentWaveIndex = 0;
+    private List<GameObject> spawnedMonsters = new List<GameObject>();
+
+    public void StartWave()
+    {
+        if (currentWaveIndex >= waveList.Count)
+        {
+            Debug.Log("모든 웨이브 클리어!");
+            return;
+        }
+
+        WaveData waveData = waveList[currentWaveIndex];
+
+        List<MonsterData> spawnQueue = WaveBuilder.BuildWave(waveData, allMonsters);
+    }
+    void OnWaveCleared()
+    {
+        WaveData waveData = waveList[currentWaveIndex];
+
+        gameManager.AddGold(waveData.CalculateGoldReward());
+
+        int? jewel = waveData.TryGetJewelReward();
+        if (jewel.HasValue)
+            gameManager.AddJewel(jewel.Value);
+
+        currentWaveIndex++;
+    }
+}
