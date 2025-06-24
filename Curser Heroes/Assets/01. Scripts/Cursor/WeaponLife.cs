d766e401947ba2e7bd41ba4fd41c7bfa11f3a957
jaehun <gnslemf1227@gmail.com>
2025-06-24 19:23:06 +0900

    add, fix 무기별 공격력/목숨, 강화 기능

---
+++ Curser Heroes/Assets/01. Scripts/Cursor/WeaponLife.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class WeaponLife : MonoBehaviour
+{
+    public WeaponData currentWeapon;       //현재 장착된 무기 데이터 
+    public int currentLives { get; private set; }   //남은 목숨 확인(외부에서 읽기 전용)
+
+    public void Init(WeaponData weaponData)       //무기 장착시 초기화
+    {
+        currentWeapon = weaponData;               //무기 정보 저장
+        currentLives = currentWeapon.maxLives;     // 목숨 초기화
+    }
+
+    public void TakeLifeDamage()            //목숨이 감소했을 때 함수
+    {
+        currentLives--;                    //목숨 -1
+        Debug.Log($"{currentWeapon.weaponName} 목숨 -1 → 현재: {currentLives}");
+
+        if (currentLives <= 0)                    
+            OnOutOfLives();                    //목숨이 0 이하가 되면 파괴 처리
+    }
+
+    private void OnOutOfLives()           //목숨이 0일 때 호출되는 함수
+    {
+        Debug.Log($"{currentWeapon.weaponName} 파괴됨 (목숨 0)");  //  목숨이 0이되면 디버그로그로 확인    
+    }
+}
---
+++ Curser Heroes/Assets/01. Scripts/Cursor/WeaponManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    [Header("선택된 무기")]
+    public WeaponData selectedWeapon;   // 인스펙터나 캐릭터 선택에서 지정
+
+    [Header("무기 시스템")]
+    public CursorWeapon cursorWeapon;
+    public WeaponLife weaponLife;
+    public WeaponUpgrade weaponUpgrade;
+
+    void Start()
+    {
+        EquipWeapon(selectedWeapon);  // 게임 시작 시 한번만 호출됨
+    }
+
+    public void EquipWeapon(WeaponData weaponData)        //무기데이터를 받아 장착하는 함수
+    {
+        cursorWeapon.SetWeapon(weaponData);   
+        weaponLife.Init(weaponData);        //무기 목숨 초기화
+        weaponUpgrade.Init(weaponData);     //무기 레벨 초기화
+
+        Debug.Log($"무기 장착 완료: {weaponData.weaponName}");
+    }
+
+    public void UpgradeWeapon()      
+    {
+        weaponUpgrade.Upgrade();      //외부에서 함수를 호출 하면 강화를 1회 한다.
+    }
+
+    public void TakeWeaponLifeDamage()  //데미지를 입었을 때 호출됨
+    {
+        weaponLife.TakeLifeDamage();            //목숨을 1개 줄이고 0이 되면 끝남
+    }
+}
