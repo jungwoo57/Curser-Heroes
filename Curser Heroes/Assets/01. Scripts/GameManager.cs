de85caf78415f8028ac8b8aad9e4664432b87754
jungwoo57 <ghkdwjddn12@gmail.com>
2025-06-27 14:41:32 +0900

    update : GameManger에 보유 무기 리스트 추가

--- Curser Heroes/Assets/01. Scripts/GameManager.cs
+++ Curser Heroes/Assets/01. Scripts/GameManager.cs
@@ -1,10 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     private int gold = 0;
     private int jewel = 0;
 
+    [SerializeField] private List<WeaponData> _hasMainWeapon; // 보유 주무기
+    public IReadOnlyList<WeaponData> hasMainWeapon => _hasMainWeapon;     // 다른 파일에서 보유 주무기 가져오기(수정 불가)
+ 
+    [SerializeField] private List<WeaponData> _hasSubWeapon; // 보유 보조무기
+    public IReadOnlyList<WeaponData> hasSubWeapon => _hasSubWeapon;     // 다른 파일에서 보유 보조 무기 가져오기(수정 불가)
+    
+    [SerializeField] private List<WeaponData> _hasPartner; // 보유 동료
+    public IReadOnlyList<WeaponData> hasPartner => _hasPartner;     // 다른 파일에서 보유 동료 가져오기(수정 불가)
     public void AddGold(int amount)
     {
         gold += amount;
@@ -24,5 +33,11 @@ public class GameManager : MonoBehaviour
     public int GetJewel()
     {
         return jewel;
+    }
+
+    public void UnlockWeapon(WeaponData weaponData)  //무기 해금 시 사용
+    {
+        // 이미 보유중인 무기면 적용 안시킬 지는 무기 해금 코드 보고 결정
+        _hasMainWeapon.Add(weaponData);              // 코드 구조 보고 보조무기 주무기 얻는 법 바꾸기
     }
 }
