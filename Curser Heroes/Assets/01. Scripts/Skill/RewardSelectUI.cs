e156357c0e1c9035699a8496b93db79b3a735f8a
kookin09 <hankookin12382@naver.com>
2025-06-27 14:12:40 +0900

    add: 스킬 선택 및 UI 관련 기초작업
    스킬 선택 및 UI 관련 기초작업 진행중

---
+++ Curser Heroes/Assets/01. Scripts/Skill/RewardSelectUI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RewardSelectUI : MonoBehaviour
+{
+    public Button healButton;
+    public Button goldButton;
+    public Button jewelButton;
+    private Action<int> onSelect;
+
+    public void Init(Action<int> callback)
+    {
+        onSelect = callback;
+
+        healButton.onClick.AddListener(() => Select(0)); // 목숨
+        goldButton.onClick.AddListener(() => Select(1));
+        jewelButton.onClick.AddListener(() => Select(2));
+    }
+
+    private void Select(int choice)
+    {
+        onSelect?.Invoke(choice);
+        Destroy(gameObject); // UI 제거
+    }
+}
--- Curser Heroes/Assets/01. Scripts/UI/StageUI/SkillUI.cs
+++ Curser Heroes/Assets/01. Scripts/UI/StageUI/SkillUI.cs
@@ -1,6 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 public class SkillUI : MonoBehaviour
@@ -8,9 +6,9 @@ public class SkillUI : MonoBehaviour
     [SerializeField] private Image skillImage;
     [SerializeField] private TextMeshProUGUI skillName;
 
-    public void UpdateSkillUI()
+    public void SetSkill(SkillData.SkillInstance instance)
     {
-        //skillImage.sprite = skillImage.sprite;   Skilldata에서 이미지 가져와서 적용
-        //skillName.text = skill.name + " : " + skill.lv;                Skilldata에서 이름 + 레벨 가져와서 적용
+        skillImage.sprite = instance.skill.icon;
+        skillName.text = $"{instance.skill.skillName} Lv.{instance.level}";
     }
 }
