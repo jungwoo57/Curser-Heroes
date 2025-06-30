9c89b9f0cbd234542ecae7b91a9b7046a452cf5e
jungwoo57 <ghkdwjddn12@gmail.com>
2025-06-30 16:46:14 +0900

    update : 보유중인 보조무기 메인 무기 UI 완성

--- Curser Heroes/Assets/01. Scripts/UI/WeaponSelectUI/WeaponImage.cs
+++ Curser Heroes/Assets/01. Scripts/UI/WeaponSelectUI/WeaponImage.cs
@@ -7,7 +7,7 @@ public class WeaponImage : MonoBehaviour
     public Image bookmarkButton; // 즐겨찾기 버튼
     public bool isBookmark;       // 북마크 체크// 이미지에 추후 합치기
     public WeaponData data;
-
+    public SubWeaponData subData;
     public static event Action OnWeaponPanelUpdate;
     
     public void WeaponUpdate(WeaponData recieveData) // 매게변수 받아야 할 확률 높음
@@ -15,7 +15,7 @@ public class WeaponImage : MonoBehaviour
         data = recieveData;
         Debug.Log("업데이트" + data);
         this.gameObject.SetActive(true);
-        if (data.weaponImage != null)
+        if (recieveData.weaponImage != null)
         {
             weaponSprite.sprite = data.weaponImage;
         }
@@ -26,10 +26,37 @@ public class WeaponImage : MonoBehaviour
         //if(isBookMark) { 북마크 색상 변경(빨간색)}
         //else{ 북마크 색상 변경 (흰색)}
     }
-
-    public void OnClickWeaponButton()
+    
+    public void WeaponUpdate(SubWeaponData recieveData) // 매게변수 받아야 할 확률 높음
     {
-        GameManager.Instance.EquipWeapon(data);
-        OnWeaponPanelUpdate?.Invoke();        //우선 메인 무기만 변경 추후 변경
+        subData = recieveData;
+        Debug.Log("업데이트" + subData);
+        this.gameObject.SetActive(true);
+        if (recieveData.weaponImage != null)
+        {
+            weaponSprite.sprite = subData.weaponImage;
+        }
+        else
+        {
+            Debug.Log("이미지 없음");
+        }
+        //if(isBookMark) { 북마크 색상 변경(빨간색)}
+        //else{ 북마크 색상 변경 (흰색)}
+    }
+
+    public void OnClickWeaponButton(int index)
+    {
+        switch (index)
+        {
+            case 0 : 
+                GameManager.Instance.EquipWeapon(data);
+                OnWeaponPanelUpdate?.Invoke();        //우선 메인 무기만 변경 추후 변경
+                break;  // 메인무기
+            case 1 : 
+                GameManager.Instance.EquipWeapon(data);  // 보조 무기 변경
+                OnWeaponPanelUpdate?.Invoke();      
+                break;  // 서브 무기
+            case 2 : break;  // 동료
+        }
     }
 }
--- Curser Heroes/Assets/01. Scripts/UI/WeaponSelectUI/WeaponScroll.cs
+++ Curser Heroes/Assets/01. Scripts/UI/WeaponSelectUI/WeaponScroll.cs
@@ -5,60 +5,97 @@ using UnityEngine.UI;
 public class WeaponScroll : MonoBehaviour
 {
     public IReadOnlyList<WeaponData> hasWeapons; // 매니저에 있는 무기 리스트 불러오기
+    public IReadOnlyList<SubWeaponData> hasSubWeapons;
     public List<WeaponImage> showWeapons = new List<WeaponImage>(); // 스크롤에서 보여줄 무기들
     public int hasWeaponCounts;
     public Transform content;   // content아래 생성 하기위해서 부모 설정
     public ScrollRect scrollRect;
     public int scrollCount;     // 일정 갯 수 이하 스크롤 x
     public GameObject weaponImagePrefabs; // 더미 데이터
-    
+
     public void UpdateScroll(string weaponType)
     {
-        hasWeapons = GameManager.Instance.hasMainWeapon; //매니저에 있는 주무기리스트 가져오기
-        hasWeaponCounts = GameManager.Instance.hasMainWeapon.Count;
-        /*switch (weaponType)
+        for (int i = 0; i < showWeapons.Count; i++)
         {
-            case "main" : 
+            showWeapons[i].gameObject.SetActive(false);
+        }
+        switch (weaponType)
+        {
+            case "main":
                 hasWeapons = GameManager.Instance.hasMainWeapon; //매니저에 있는 주무기리스트 가져오기
                 hasWeaponCounts = GameManager.Instance.hasMainWeapon.Count;
+                if (hasWeapons.Count > scrollCount) // 아이템이 일정 갯수 이하이면 스크롤 안되게 하기
+                {
+                    scrollRect.vertical = true;
+                }
+                else
+                {
+                    scrollRect.vertical = false;
+                }
+
+                for (int i = 0; i < hasWeaponCounts; i++)
+                {
+                    Debug.Log(hasWeapons[i].weaponName);
+                }
+
+                while (showWeapons.Count < hasWeapons.Count) //갯수 부족 할 시 동적생성
+                {
+                    GameObject obj = Instantiate(weaponImagePrefabs, content);
+                    WeaponImage weaponImage = obj.GetComponent<WeaponImage>();
+                    showWeapons.Add(weaponImage);
+                }
+
+                for (int i = 0; i < hasWeaponCounts; i++)
+                {
+                    showWeapons[i].WeaponUpdate(hasWeapons[i]); // WeaponImage 업데이트
+                }
+
+                for (int i = hasWeapons.Count; i < showWeapons.Count; i++)
+                {
+                    showWeapons[i].gameObject.SetActive(false); // 남은 부분 끄기
+                }
+
                 break;
-            case "sub" : 
-                hasWeapons = GameManager.Instance.hasSubWeapon; //매니저에 있는 서브리스트 가져오기
+
+            case "sub":
+                hasSubWeapons = GameManager.Instance.hasSubWeapon; //매니저에 있는 서브리스트 가져오기
                 hasWeaponCounts = GameManager.Instance.hasSubWeapon.Count;
+                if (hasSubWeapons.Count > scrollCount) // 아이템이 일정 갯수 이하이면 스크롤 안되게 하기
+                {
+                    scrollRect.vertical = true;
+                }
+                else
+                {
+                    scrollRect.vertical = false;
+                }
+
+                for (int i = 0; i < hasWeaponCounts; i++)
+                {
+                    Debug.Log(hasWeapons[i].weaponName);
+                }
+
+                while (showWeapons.Count < hasSubWeapons.Count) //갯수 부족 할 시 동적생성
+                {
+                    GameObject obj = Instantiate(weaponImagePrefabs, content);
+                    WeaponImage weaponImage = obj.GetComponent<WeaponImage>();
+                    showWeapons.Add(weaponImage);
+                }
+
+                for (int i = 0; i < hasWeaponCounts; i++)
+                {
+                    showWeapons[i].WeaponUpdate(hasSubWeapons[i]); // WeaponImage 업데이트
+                }
+
+                for (int i = hasWeapons.Count; i < showWeapons.Count; i++)
+                {
+                    showWeapons[i].gameObject.SetActive(false); // 남은 부분 끄기
+                }
 
                 break;
-            case "partner" : 
+
+            case "partner":
                 break;
-        }*/
-
-        if (hasWeapons.Count > scrollCount) // 아이템이 일정 갯수 이하이면 스크롤 안되게 하기
-        {
-            scrollRect.vertical = true;
-        }
-        else
-        {
-            scrollRect.vertical = false;
-        }
-        
-        for (int i = 0; i < hasWeaponCounts; i++)
-        {
-            Debug.Log(hasWeapons[i].weaponName);
-        }
-
-        while (showWeapons.Count < hasWeapons.Count) //갯수 부족 할 시 동적생성
-        {
-            GameObject obj = Instantiate(weaponImagePrefabs, content);
-            WeaponImage weaponImage = obj.GetComponent<WeaponImage>();
-            showWeapons.Add(weaponImage);
-        }
-        for(int i = 0 ; i < hasWeaponCounts; i++)
-         {
-              showWeapons[i].WeaponUpdate(hasWeapons[i]);          // WeaponImage 업데이트
-         }
-         for(int i = hasWeapons.Count; i < showWeapons.Count; i++)
-         {
-              showWeapons[i].gameObject.SetActive(false);       // 남은 부분 끄기
-         }
         }
     }
+}
 
