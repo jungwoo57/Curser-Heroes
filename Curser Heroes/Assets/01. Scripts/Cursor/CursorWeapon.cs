384c41d5fe11ebe2a49ffe803400328c1b226488
jellujoa <jellujoa03@gmail.com>
2025-07-01 22:04:08 +0900

    update boss

--- Curser Heroes/Assets/01. Scripts/Cursor/CursorWeapon.cs
+++ Curser Heroes/Assets/01. Scripts/Cursor/CursorWeapon.cs
@@ -8,9 +8,11 @@ public class CursorWeapon : MonoBehaviour
     public WeaponLife weaponLife;       // 분리된 목숨 관리 
     public WeaponUpgrade weaponUpgrade;      // 무기 레벨 관리
 
-    private Dictionary<BaseMonster, float> lastHitTimes = new();       
+    private Dictionary<BaseMonster, float> lastHitTimesBase = new Dictionary<BaseMonster, float>();
+    private Dictionary<BossBaseMonster, float> lastHitTimesBoss = new Dictionary<BossBaseMonster, float>();
+
     //공격 쿨타임을 위해 몬스터 별로 마지막 공격한 시간을 저장, 몬스터 마다 각각 쿨타임을 적용할 수 있다.
-    
+
     private Camera cam;      // 마우스 좌표를 월드 좌표로 바꾸기 위해 메인 카메라를 참조.
 
     void Start()
@@ -38,22 +40,39 @@ public class CursorWeapon : MonoBehaviour
 
         Collider2D[] hits = Physics2D.OverlapCircleAll(cursorPos, range, targetLayer);   // 커서 위치를 중심으로 원으로 범위 탐지
 
-        foreach (var hit in hits)    //감지된 콜라이더에 대한 반복
+        foreach (var hit in hits)
         {
-            BaseMonster monster = hit.GetComponent<BaseMonster>();    // 해당 오브젝트에 BaseMonster 컴포넌트가 없으면 무시
-
-           
-            if (monster == null) continue;
-
-            if (!lastHitTimes.TryGetValue(monster, out float lastHitTime))//해당 몬스터가 딕셔너리에 있는지 없는지 체그해서 마지막 공격시간 확인
-                lastHitTime = 0f;
-
-            if (Time.time - lastHitTime >= cooldown)            //쿨타임이 지났다면 공격 실행 
+            // 일반 몬스터 감지
+            BaseMonster monster = hit.GetComponent<BaseMonster>();
+            if (monster != null)
             {
-                monster.TakeDamage(Mathf.RoundToInt(damage));    // 소수점 공격력을 정수화 해서 데미지를 전달
-                lastHitTimes[monster] = Time.time;    //공격 시점 갱신
+                if (!lastHitTimesBase.TryGetValue(monster, out float lastHitTime))
+                    lastHitTime = 0f;
+
+                if (Time.time - lastHitTime >= cooldown)
+                {
+                    monster.TakeDamage(Mathf.RoundToInt(damage));
+                    lastHitTimesBase[monster] = Time.time;
+                }
+                continue;
+            }
+
+            // 보스 몬스터 감지
+            BossBaseMonster boss = hit.GetComponent<BossBaseMonster>();
+            if (boss != null)
+            {
+                if (!lastHitTimesBoss.TryGetValue(boss, out float lastHitTime))
+                    lastHitTime = 0f;
+
+                if (Time.time - lastHitTime >= cooldown)
+                {
+                    boss.TakeDamage(Mathf.RoundToInt(damage));
+                    lastHitTimesBoss[boss] = Time.time;
+                }
             }
         }
+
+
     }
     public void SetWeapon(WeaponData weaponData)     //외부에서 무기를 장착할 수 있게 해주는 초기화 함수
     {
