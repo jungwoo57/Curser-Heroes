9919aece320325003574a1fa4ef438022715744b
jungwoo57 <ghkdwjddn12@gmail.com>
2025-07-02 20:16:00 +0900

    update : WeaponData 레벨 및 업그레이드 비용 추가

--- Curser Heroes/Assets/01. Scripts/UI/ForgeUI/ForgeUI.cs
+++ Curser Heroes/Assets/01. Scripts/UI/ForgeUI/ForgeUI.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -5,12 +6,25 @@ public class ForgeUI : MonoBehaviour
 {
     public Image weaponImage;
     public Image WeaponTextInfo;
+    
+    [Header("텍스트 목록")]
     public TextMeshProUGUI hasGoldText;
     public TextMeshProUGUI useGoldText;
+    public TextMeshProUGUI weaponName;
+    public TextMeshProUGUI weaponDesc;
+    public TextMeshProUGUI weaponHP;
+    public TextMeshProUGUI weaponAtk;
+    
     public Button reinforceButton;
+    public WeaponData selectWeapon;
 
-    
-    
+    public void Init()
+    {
+        selectWeapon = GameManager.Instance.mainEquipWeapon;
+        
+        TextUpdate();
+    }
+
     public void DisableReinforceButton()
     {
         reinforceButton.interactable = false;
@@ -19,5 +33,20 @@ public class ForgeUI : MonoBehaviour
     public void OnClickReinforceButton()
     {
         Debug.Log("무기강화");       //weapondata에 레벨이 존재해야 할 것 같음
+    }
+
+    public void TextUpdate()
+    {
+        weaponName.text = selectWeapon.weaponName;
+        weaponDesc.text = selectWeapon.weaponDesc;
+        weaponAtk.text = selectWeapon.baseDamage.ToString();
+        weaponHP.text = selectWeapon.maxLives.ToString();
+        hasGoldText.text = GameManager.Instance.GetGold().ToString();
+        //useGoldText.text  업그레이드 비용 weaponData에 필요 
+    }
+
+    public void ImageUpdate()
+    {
+        weaponImage.sprite = selectWeapon.weaponImage;
     }
 }
