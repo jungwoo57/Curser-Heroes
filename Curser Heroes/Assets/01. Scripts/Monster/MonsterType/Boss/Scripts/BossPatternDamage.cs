d106c8184877ccddd74fc9bf2186294d3bbb5362
jellujoa <jellujoa03@gmail.com>
2025-07-07 20:26:27 +0900

    Revert "Update BossPatternDamage.cs"
    This reverts commit 9e15411e8e4b6f7d843e0cd1fb519c9a4291970f.

--- Curser Heroes/Assets/01. Scripts/Monster/MonsterType/Boss/Scripts/BossPatternDamage.cs
+++ Curser Heroes/Assets/01. Scripts/Monster/MonsterType/Boss/Scripts/BossPatternDamage.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class BossPatternDamage : MonoBehaviour
 {
     private Collider2D col;
-    public float cooldownTime = 1; // 공격 쿨타임 (초 단위)
-
+    
 
     private void Awake()
     {
@@ -23,15 +22,9 @@ public class BossPatternDamage : MonoBehaviour
     }
     private void Update()
     {
-      
-       
         if (col.enabled)
         {
-            if (cooldownTime <= 0)
-            {
-                Attack(); // 콜라이더가 활성화 되어 있으면 공격 함수 호출
-            }
-                      
+            Attack(); // 콜라이더가 활성화 되어 있으면 공격 함수 호출
         }
     }
 
@@ -51,16 +44,4 @@ public class BossPatternDamage : MonoBehaviour
             }
         }
     }
-    public void cooltime()
-    {
-        if (cooldownTime > 0)
-        {
-            cooldownTime -= Time.deltaTime; // 쿨타임 감소
-            if (cooldownTime <= 0)
-            {
-                cooldownTime = 1; // 쿨타임이 0 이하로 내려가면 0으로 설정
-            }
-        }
-    }
-    
 }
