0bc041c78a497642eef6fbea2eee236d60bfd2a1
jellujoa <jellujoa03@gmail.com>
2025-07-08 18:25:30 +0900

    update pattern damage

--- Curser Heroes/Assets/01. Scripts/Partner/Partner/Hunter.cs
+++ Curser Heroes/Assets/01. Scripts/Partner/Partner/Hunter.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Hunter : BasePartner
 {
     public float skillRange = 20f;
-
+    BaseMonster baseMonster;
     private void Update()
     {
         if(currentGauge >= data.gaugeMax)
@@ -16,17 +16,17 @@ public class Hunter : BasePartner
     }
     protected override void ActivateSkill()
     {
-        Collider2D weaponCollider = Physics2D.OverlapCircle(transform.position, skillRange, LayerMask.GetMask("Weapon"));
-        if (weaponCollider != null)
+        Collider2D monsterCollider = Physics2D.OverlapCircle(transform.position, skillRange, LayerMask.GetMask("Monster"));
+        if (monsterCollider != null)
         {
-            if (WeaponManager.Instance != null)
+            if (baseMonster != null)
             {
-                WeaponManager.Instance.TakeWeaponLifeDamage();
-                Debug.Log("스킬발동 ! ");
+                baseMonster.TakeDamage(1,null);
+                Debug.Log($"Hunter 스킬 발동: {baseMonster.name}에게 1의 피해를 입혔습니다.");
             }
             else
             {
-                Debug.LogWarning("WeaponManager 인스턴스를 찾을 수 없습니다!");
+                Debug.LogWarning("monsterCollider를 찾을 수 없습니다!");
             }
         }
     }
