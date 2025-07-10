76b5224540c01118b55935af63de826a7e4b28b1
kookin09 <hankookin12382@naver.com>
2025-07-10 16:28:44 +0900

    test: 구원,근력 훈련 스킬 테스트 중
    구원,근력 훈련 스킬 테스트 중

---
+++ Curser Heroes/Assets/01. Scripts/Skill/Script/StrengthTrainingSkill.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class StrengthTrainingSkill : MonoBehaviour
+{
+    private SkillManager.SkillInstance skillInstance;
+
+    public void Init(SkillManager.SkillInstance instance)
+    {
+        skillInstance = instance;
+    }
+
+    // 현재 레벨에 따른 데미지 증가량 반환
+    public int GetDamageIncrease()
+    {
+        if (skillInstance == null) return 0;
+
+        int levelIndex = Mathf.Clamp(skillInstance.level - 1, 0, skillInstance.skill.levelDataList.Count - 1);
+        return skillInstance.skill.levelDataList[levelIndex].damage;
+    }
+}
