be00b9ab02b3dce8eb3521393481731cb0a7ced7
jellujoa <jellujoa03@gmail.com>
2025-07-16 14:57:55 +0900

    Add GreenSlime , PoisonMushroom

--- Curser Heroes/Assets/01. Scripts/Monster/MonsterType/GreenSlime/GreenSlime.cs
+++ Curser Heroes/Assets/01. Scripts/Monster/MonsterType/GreenSlime/GreenSlime.cs
@@ -1,18 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
 
-public class GreenSlime : MonoBehaviour
+public class GreenSlime: BaseMonster
 {
-    // Start is called before the first frame update
-    void Start()
+    public float attackRange = 0.5f;
+
+    protected override void Attack()
     {
-        
+        Collider2D weaponCollider = Physics2D.OverlapCircle(transform.position, attackRange, LayerMask.GetMask("Weapon"));
+        if (weaponCollider != null)
+        {
+            if (WeaponManager.Instance != null)
+            {
+                WeaponManager.Instance.TakeWeaponLifeDamage();
+                Debug.Log("근접 공격으로 무기 내구도 감소!");
+            }
+            else
+            {
+                Debug.LogWarning("WeaponManager 인스턴스를 찾을 수 없습니다!");
+            }
+        }
+        StartCoroutine(WaitForAnimationEnd("Attack")); // 애니메이션이 끝날 때까지 대기
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    private void OnDrawGizmosSelected()
     {
-        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+    private IEnumerator WaitForAnimationEnd(string stateName, float timeout = 5f)
+    {
+        int layer = 0;
+        float start = Time.time;
+        while (!animator.GetCurrentAnimatorStateInfo(layer).IsName(stateName))
+        {
+            if (Time.time - start > timeout)
+            {
+                Destroy(gameObject);
+            }
+            yield return null;
+        }
+        while (animator.GetCurrentAnimatorStateInfo(layer).normalizedTime < 1f)
+        {
+            if (Time.time - start > timeout)
+            {
+
+                Destroy(gameObject);
+            }
+            yield return null;
+        }
     }
 }
