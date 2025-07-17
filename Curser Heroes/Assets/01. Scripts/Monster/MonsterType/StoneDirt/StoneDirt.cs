ec2d052703cf3dfb64f1a736f6de8d79c9e8f279
jellujoa <jellujoa03@gmail.com>
2025-07-17 16:52:43 +0900

    update monster

---
+++ Curser Heroes/Assets/01. Scripts/Monster/MonsterType/StoneDirt/StoneDirt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneDirt : BaseMonster
+{
+    public GameObject projectilePrefab; // 투사체 프리팹
+    public Transform firePoint;         // 발사 위치
+    public float attackRange = 5f;      // 공격 사거리
+    protected override void Attack()
+    {
+        Collider2D weaponCollider = Physics2D.OverlapCircle(transform.position, attackRange, LayerMask.GetMask("Weapon"));
+        if (weaponCollider == null) return;
+
+        if (projectilePrefab != null && firePoint != null)
+        {
+            Vector3 direction = (weaponCollider.transform.position - firePoint.position).normalized;
+            GameObject projectile = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity);
+            DirtProjectile projScript = projectile.GetComponent<DirtProjectile>();
+            if (projScript != null)
+                projScript.Initialize(direction, damage);
+
+        }
+
+
+        Debug.Log("원거리 몬스터: 투사체 발사!");
+
+    }
+}
