17e807bf88db72d52ef54da22a8a8540aee072ef
kookin09 <hankookin12382@naver.com>
2025-07-17 21:00:01 +0900

    add: 동료 소식을 전하는새 추가
    동료 소식을 전하는새 추가

--- Curser Heroes/Assets/01. Scripts/Partner/Partner/Messengerbird/MessengerbirdEffect.cs
+++ Curser Heroes/Assets/01. Scripts/Partner/Partner/Messengerbird/MessengerbirdEffect.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Messengerbird: MonoBehaviour
+public class MessengerbirdEffect : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
