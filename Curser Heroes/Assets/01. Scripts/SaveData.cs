67b8cf875c3a312664773216e866f3cddb3509b8
kookin09 <hankookin12382@naver.com>
2025-08-01 16:12:25 +0900

    머지 충돌 부분 수정
    머지 충돌 부분 수정

--- Curser Heroes/Assets/01. Scripts/SaveData.cs
+++ Curser Heroes/Assets/01. Scripts/SaveData.cs
@@ -1,16 +1,16 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
 public class SaveData
 {
-    public List<SkillData> hasSkills;
+    public List<SkillData> unlockedSkills;
+    public List<SkillData> selectedSkills;
     public List<OwnedWeapon> ownedWeapons;
     public List<OwnedSubWeapon> ownedSubWeapons;
     public OwnedWeapon mainEquipWeapon;
     public OwnedSubWeapon subEquipWeapon;
-    public List<SkillData> selectedSkills;
 
     public int stage1bestWave;
     public int stage2bestWave;
