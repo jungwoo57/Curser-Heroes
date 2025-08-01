a9ad2afe0b623272c8e8e0c7c805505077288abc
kookin09 <hankookin12382@naver.com>
2025-08-01 16:01:14 +0900

    fix: 사용가능 스킬 로드 방식 변경
    사용가능 스킬 로드 방식 변경

--- Curser Heroes/Assets/01. Scripts/SaveData.cs
+++ Curser Heroes/Assets/01. Scripts/SaveData.cs
@@ -1,16 +1,16 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
 public class SaveData
 {
-    public List<SkillData> hasSkills;
+    public List<SkillData> unlockedSkills;
+    public List<SkillData> selectedSkills;
     public List<OwnedWeapon> ownedWeapons;
     public List<OwnedSubWeapon> ownedSubWeapons;
     public OwnedWeapon mainEquipWeapon;
     public OwnedSubWeapon subEquipWeapon;
-    public List<SkillData> selectedSkills;
     
     public int gold;
     public int jewel;
