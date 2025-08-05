ece9ff5b95757cc1a580b12cfa83ebe1585094ae
jaehun <gnslemf1227@gmail.com>
2025-08-05 20:53:31 +0900

    update 서브웨폰 오디오 추가

--- Curser Heroes/Assets/01. Scripts/Cursor/SubWeapon/SubProjectile.cs
+++ Curser Heroes/Assets/01. Scripts/Cursor/SubWeapon/SubProjectile.cs
@@ -3,16 +3,12 @@
 [RequireComponent(typeof(Collider2D), typeof(Rigidbody2D))]
 public class SubProjectile : MonoBehaviour
 {
-    
     [SerializeField] protected SubWeaponData subWeaponData;
     [SerializeField] protected int calculatedDamage;
     protected BaseMonster target;
     protected float speed;
 
-  
     public SubWeaponData WeaponData => subWeaponData;
-
-    
     public int DamageAmount =>
         (calculatedDamage > 0)
             ? calculatedDamage
@@ -36,11 +32,8 @@ public class SubProjectile : MonoBehaviour
 
     private void Awake()
     {
-        // Collider를 Trigger로 전환
         var col = GetComponent<Collider2D>();
         col.isTrigger = true;
-
-        // Rigidbody를 Kinematic으로 설정
         var rb = GetComponent<Rigidbody2D>();
         rb.isKinematic = true;
         rb.gravityScale = 0f;
@@ -57,15 +50,13 @@ public class SubProjectile : MonoBehaviour
         transform.position += dir * speed * Time.deltaTime;
     }
 
-
-    protected virtual void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.TryGetComponent<BaseMonster>(out var monster) && !monster.IsDead)
         {
             Debug.Log($"[SubProjectile] Hit {monster.name}, dmg = {DamageAmount}");
             monster.TakeDamage(DamageAmount, subWeaponData);
 
-            // 상태이상 효과: Burn 또는 Stun 중 하나
             switch (subWeaponData.effect)
             {
                 case SubWeaponEffect.Burn:
@@ -73,14 +64,16 @@ public class SubProjectile : MonoBehaviour
                         monster,
                         subWeaponData.burnDamagePerSecond,
                         subWeaponData.burnDuration,
-                        1f  // 1초마다 Tick
+                        1f
                     );
+                   
                     monster.GetComponent<EffectManager>()?.AddEffect(burn);
                     break;
 
                 case SubWeaponEffect.Stun:
-                    // StunEffect 생성자에 지속시간만 전달
                     var stun = new StunEffect(subWeaponData.stunDuration);
+                    
+                    stun.Apply(monster);
                     monster.GetComponent<EffectManager>()?.AddEffect(stun);
                     break;
             }
@@ -88,6 +81,5 @@ public class SubProjectile : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
 
 }
